a0d1643b0991127bb7738b4c13f7216ea32eb991
19Hamid91 <99725605+19Hamid91@users.noreply.github.com>
2024-07-17 08:26:10 +0700

    fix: hide kartu penyusutan where doesnt have pembelian

--- app/Http/Controllers/KartuPenyusutanController.php
+++ app/Http/Controllers/KartuPenyusutanController.php
@@ -23,7 +23,7 @@ class KartuPenyusutanController extends Controller
     public function index($instansi)
     {
         $data_instansi = Instansi::where('nama_instansi', $instansi)->first();
-        $asets = KartuPenyusutan::whereHas('aset', function($q) use($data_instansi){
+        $asets = KartuPenyusutan::wwhereHas('pembelian_aset')->hereHas('aset', function($q) use($data_instansi){
             $q->where('instansi_id', $data_instansi->id);
         })->orderByDesc('id')->with('aset', 'pembelian_aset', 'komponen')->get();
         $akun = Akun::where('instansi_id', $data_instansi->id)->get();
